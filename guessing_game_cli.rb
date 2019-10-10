db330993b9a3addebcecdfd51610dbad1af301a5
Jordan Long <jordanleelong5@gmail.com>
2019-10-10 01:08:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ num = 1+rand(6)
 puts "Guess a number between 1 and 6"
 user_input = gets.chomp
 if user_input == num.to_s
-  puts "You guessed the correct number"
+  puts "You guessed the correct number!"
   elsif user_input == "exit"
     puts "Goodbye!"
   else
